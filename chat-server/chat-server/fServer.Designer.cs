7da9019ccc95bf406f8398d0691be1633d450670
Thien Tran <rimc2t>
2020-06-08 16:50:20 +0700

    upload solutions

---
+++ chat-server/chat-server/fServer.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace chat_server
+{
+	partial class fServer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtStatus = new System.Windows.Forms.RichTextBox();
+			this.txtCmd = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnClose = new System.Windows.Forms.Button();
+			this.btnHide = new System.Windows.Forms.Button();
+			this.pnHeader = new System.Windows.Forms.Panel();
+			this.pnHeader.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// txtStatus
+			// 
+			this.txtStatus.Enabled = false;
+			this.txtStatus.Location = new System.Drawing.Point(12, 41);
+			this.txtStatus.Name = "txtStatus";
+			this.txtStatus.Size = new System.Drawing.Size(560, 282);
+			this.txtStatus.TabIndex = 0;
+			this.txtStatus.Text = "";
+			// 
+			// txtCmd
+			// 
+			this.txtCmd.Location = new System.Drawing.Point(12, 329);
+			this.txtCmd.Name = "txtCmd";
+			this.txtCmd.Size = new System.Drawing.Size(560, 20);
+			this.txtCmd.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+			this.label1.Location = new System.Drawing.Point(7, 3);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(134, 28);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Chat Server";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// btnClose
+			// 
+			this.btnClose.BackColor = System.Drawing.Color.Maroon;
+			this.btnClose.ForeColor = System.Drawing.Color.White;
+			this.btnClose.Location = new System.Drawing.Point(540, 1);
+			this.btnClose.Name = "btnClose";
+			this.btnClose.Size = new System.Drawing.Size(33, 33);
+			this.btnClose.TabIndex = 3;
+			this.btnClose.Text = "X";
+			this.btnClose.UseVisualStyleBackColor = false;
+			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+			// 
+			// btnHide
+			// 
+			this.btnHide.BackColor = System.Drawing.Color.Snow;
+			this.btnHide.ForeColor = System.Drawing.Color.Black;
+			this.btnHide.Location = new System.Drawing.Point(508, 1);
+			this.btnHide.Name = "btnHide";
+			this.btnHide.Size = new System.Drawing.Size(33, 33);
+			this.btnHide.TabIndex = 4;
+			this.btnHide.Text = "__";
+			this.btnHide.UseVisualStyleBackColor = false;
+			this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+			// 
+			// pnHeader
+			// 
+			this.pnHeader.BackColor = System.Drawing.Color.Teal;
+			this.pnHeader.Controls.Add(this.label1);
+			this.pnHeader.Controls.Add(this.btnHide);
+			this.pnHeader.Controls.Add(this.btnClose);
+			this.pnHeader.Location = new System.Drawing.Point(0, 0);
+			this.pnHeader.Name = "pnHeader";
+			this.pnHeader.Size = new System.Drawing.Size(584, 35);
+			this.pnHeader.TabIndex = 5;
+			this.pnHeader.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnHeader_MouseDown);
+			this.pnHeader.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnHeader_MouseMove);
+			this.pnHeader.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnHeader_MouseUp);
+			// 
+			// fServer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.MediumTurquoise;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.ClientSize = new System.Drawing.Size(584, 361);
+			this.Controls.Add(this.pnHeader);
+			this.Controls.Add(this.txtCmd);
+			this.Controls.Add(this.txtStatus);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "fServer";
+			this.Text = "CHAT SERVER";
+			this.Load += new System.EventHandler(this.fServer_Load);
+			this.pnHeader.ResumeLayout(false);
+			this.pnHeader.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox txtStatus;
+		private System.Windows.Forms.TextBox txtCmd;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnClose;
+		private System.Windows.Forms.Button btnHide;
+		private System.Windows.Forms.Panel pnHeader;
+	}
+}
+
---
+++ chat-server/chat-server/fServer.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace chat_server
+{
+	public partial class fServer : Form
+	{
+		//Tạo socket
+		private Socket serverSocket = new Socket(
+			AddressFamily.InterNetwork,
+			SocketType.Stream,
+			ProtocolType.Tcp);
+		//Tạo endpoint
+		private IPEndPoint ep = new IPEndPoint(IPAddress.Any, 1302);
+
+		//Khởi tạo kết nối database
+		string dbHost;
+		string dbName;
+		string dbUsername;
+		string dbPassword;
+		string conStr;
+		MySqlConnection con;
+
+		public fServer()
+		{
+			InitializeComponent();
+		}
+
+		private void fServer_Load(object sender, EventArgs e)
+		{
+			txtStatus.AppendText("Setting up Server ...\n");
+
+			//Open Database			
+			connectDatabase();
+			txtStatus.AppendText("Database connected!\n");
+			try
+			{
+				con.Open();
+				txtStatus.AppendText("Database is openning...\n");
+			}
+			catch (Exception ex)
+			{
+				txtStatus.AppendText("ERROR: " + ex.Message + "\n");
+			}
+
+
+			//Server Thread
+			CheckForIllegalCrossThreadCalls = false;
+			Thread serverThread = new Thread(proceedServer);
+			try
+			{
+				serverThread.Start();
+			}
+			catch (Exception ex)
+			{
+				txtStatus.AppendText("ERROR: " + ex.Message + "\n");
+			}
+		}
+
+		private void connectDatabase()
+		{
+			txtStatus.AppendText("Connecting database ...\n");
+			dbHost = "localhost";
+			dbName = "chat_app_wfrm"; 
+			dbUsername = "root";
+			dbPassword = "1302";
+			conStr =
+				"SERVER=" + dbHost +
+				";DATABASE=" + dbName +
+				";uid=" + dbUsername +
+				";pwd=" + dbPassword + ";";
+			con = new MySqlConnection(conStr);
+		}
+
+		private void proceedServer()
+		{
+			//Liên kết cổng sk với endpoint
+			serverSocket.Bind(ep);
+			txtStatus.AppendText("Server is already running ...\n");
+			//Lắng nghe kết nối với hàng đợi tối đa 10
+			serverSocket.Listen(100);
+			txtStatus.AppendText("Server is listening ...\n");
+			while (true)
+			{
+				try
+				{
+					Socket clientSocket = serverSocket.Accept();
+					Thread clientThread = new Thread(proceedThread);
+					clientThread.Start(clientSocket);
+				}
+				catch (Exception ex)
+				{
+					txtStatus.AppendText("ERROR: " + ex.Message + "\n");
+					break;
+				}
+			}
+		}
+
+		private void proceedThread(object state)
+		{
+			Socket acceptedClientSocket = (Socket)state;
+			txtStatus.AppendText("A client connected!\n");
+			try
+			{
+				while (true)
+				{
+					byte[] recData = new byte[1024];
+					string strData = "";
+					acceptedClientSocket.Receive(recData);
+					strData = Encoding.UTF8.GetString(recData);
+				}
+			}
+			catch(Exception ex)
+			{
+				txtStatus.AppendText("ERROR: " + ex.Message + "\n");
+				acceptedClientSocket.Close();
+				Thread.CurrentThread.Abort();
+			}
+		}
+
+		//Move winform
+		private int mov;
+		private int movX;
+		private int movY;
+
+		private void pnHeader_MouseDown(object sender, MouseEventArgs e)
+		{
+			mov = 1;
+			movX = e.X;
+			movY = e.Y;
+		}
+
+		private void pnHeader_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (mov == 1)
+			{
+				this.SetDesktopLocation(MousePosition.X - movX, MousePosition.Y - movY);
+			}
+		}
+
+		private void pnHeader_MouseUp(object sender, MouseEventArgs e)
+		{
+			mov = 0;
+		}
+
+		private void btnClose_Click(object sender, EventArgs e)
+		{
+			serverSocket.Close();
+			con.Close();
+			this.Close();
+		}
+
+		private void btnHide_Click(object sender, EventArgs e)
+		{
+			this.WindowState = FormWindowState.Minimized;
+		}
+	}
+}
