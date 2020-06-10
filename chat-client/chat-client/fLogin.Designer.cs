d225b0249e37ddfe1f24afb21ed67e35df5fe822
RimCT <tranchithienc2t@gmail.com>
2020-06-10 14:06:38 +0700

    client

--- chat-client/chat-client/fLogin.Designer.cs
+++ chat-client/chat-client/fLogin.Designer.cs
@@ -28,20 +28,142 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnHide = new System.Windows.Forms.Button();
+			this.btnClose = new System.Windows.Forms.Button();
+			this.txtUsername = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.btnSubmit = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.btnHide);
+			this.panel1.Controls.Add(this.btnClose);
+			this.panel1.ForeColor = System.Drawing.Color.OldLace;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(300, 45);
+			this.panel1.TabIndex = 0;
+			this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnHeader_MouseDown);
+			this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnHeader_MouseMove);
+			this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnHeader_MouseUp);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Playbill", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.MediumTurquoise;
+			this.label1.Location = new System.Drawing.Point(3, 3);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(117, 40);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "CHAT APP";
+			// 
+			// btnHide
+			// 
+			this.btnHide.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.btnHide.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.btnHide.Location = new System.Drawing.Point(220, 0);
+			this.btnHide.Name = "btnHide";
+			this.btnHide.Size = new System.Drawing.Size(40, 40);
+			this.btnHide.TabIndex = 1;
+			this.btnHide.Text = "__";
+			this.btnHide.UseVisualStyleBackColor = false;
+			this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+			// 
+			// btnClose
+			// 
+			this.btnClose.BackColor = System.Drawing.Color.Brown;
+			this.btnClose.Location = new System.Drawing.Point(260, 0);
+			this.btnClose.Name = "btnClose";
+			this.btnClose.Size = new System.Drawing.Size(40, 40);
+			this.btnClose.TabIndex = 1;
+			this.btnClose.Text = "X";
+			this.btnClose.UseVisualStyleBackColor = false;
+			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+			// 
+			// txtUsername
+			// 
+			this.txtUsername.BackColor = System.Drawing.SystemColors.Window;
+			this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtUsername.Location = new System.Drawing.Point(61, 135);
+			this.txtUsername.Name = "txtUsername";
+			this.txtUsername.Size = new System.Drawing.Size(182, 27);
+			this.txtUsername.TabIndex = 0;
+			this.txtUsername.Text = "username";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Playbill", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+			this.label2.Location = new System.Drawing.Point(96, 67);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(99, 40);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "SIGN IN";
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(61, 188);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.PasswordChar = '*';
+			this.textBox1.Size = new System.Drawing.Size(182, 27);
+			this.textBox1.TabIndex = 4;
+			this.textBox1.Text = "password";
+			// 
+			// btnSubmit
+			// 
+			this.btnSubmit.BackColor = System.Drawing.Color.Honeydew;
+			this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnSubmit.ForeColor = System.Drawing.Color.DarkSlateGray;
+			this.btnSubmit.Location = new System.Drawing.Point(86, 246);
+			this.btnSubmit.Name = "btnSubmit";
+			this.btnSubmit.Size = new System.Drawing.Size(127, 45);
+			this.btnSubmit.TabIndex = 5;
+			this.btnSubmit.Text = "SUBMIT";
+			this.btnSubmit.UseVisualStyleBackColor = false;
 			// 
 			// fLogin
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(584, 361);
+			this.BackColor = System.Drawing.Color.MediumTurquoise;
+			this.ClientSize = new System.Drawing.Size(300, 400);
+			this.Controls.Add(this.btnSubmit);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtUsername);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Name = "fLogin";
 			this.Text = "LOGIN";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.TextBox txtUsername;
+		private System.Windows.Forms.Button btnClose;
+		private System.Windows.Forms.Button btnHide;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button btnSubmit;
 	}
 }
 
