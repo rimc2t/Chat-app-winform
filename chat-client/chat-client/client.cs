d085d7dc162a8d4e68f74aab6cf0e6b7e9631f1f
RimCT <tranchithienc2t@gmail.com>
2020-06-12 10:22:12 +0700

    test push

--- chat-client/chat-client/client.cs
+++ chat-client/chat-client/client.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
 
 namespace chat_client
 {
 	class Client
 	{
+
+		public Client()
+		{
+			int a = 100;
+		}
+
 	}
 }
